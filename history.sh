ae72e319fcefdb1fd1f9009e646c144a1ee25f6a
Mara Kim <hacker.root@gmail.com>
2013-04-02 16:44:03 -0500

    added history.sh

---
+++ history.sh
@@ -0,0 +1,50 @@
+# history
+# persistent shell history with advanced search
+#
+# Copyright (C) 2013 Mara Kim, Kris McGary
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see http://www.gnu.org/licenses/.
+
+
+### USAGE ###
+# Source this file in your shell's .*rc file
+
+
+#set up history logging of commands
+export HISTTIMEFORMAT='%F %T '
+PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND ; }"'echo $USER $PWD "$(history 1)" >> ~/.bash_all_history'
+
+#grep history
+function hg { grep $1 ~/.bash_all_history; }
+
+#history of commands run in this directory and subdirectories (with grep)
+function dh {
+if [ $1 ]
+then
+    grep "$PWD" ~/.bash_all_history | grep $1
+else
+    grep "$PWD" ~/.bash_all_history
+fi
+}
+
+#history of commands run in this directory only (with grep)
+function ldh {
+if [ $1 ]
+then
+    grep "$PWD " ~/.bash_all_history | grep $1
+else
+    grep "$PWD " ~/.bash_all_history
+fi
+}
+
