279b1b92845977ac0797151811ee20bb8773a66b
Mara Kim <hacker.root@gmail.com>
2013-05-07 17:15:56 -0500

    quoting

--- history.sh
+++ history.sh
@@ -23,7 +23,7 @@
 
 #set up history logging of commands
 export HISTTIMEFORMAT='%F %T '
-PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND ; }"'echo $USER $(readlink -e $PWD) "$(history 1 | cut -d " " -f4-)" >> ~/.bash_all_history'
+PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND ; }"'echo "$USER" "$(readlink -e $PWD)" "$(history 1 | cut -d " " -f4-)" >> ~/.bash_all_history'
 
 #grep history
 function gh {
